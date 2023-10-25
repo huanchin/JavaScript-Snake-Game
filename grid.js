240b5518143875d6bbea43cc6eb5107e286e045e
huan-chin chen <cherry032826@gmail.com>
2023-10-25 08:23:33 +0800

    [Fix] fix random position of food

--- grid.js
+++ grid.js
@@ -1,19 +1,9 @@
 const GRID_SIZE = 21;
 
-function getRandomNumberA(quantity_of_nums) {
-  let milliseconds = new Date().getMilliseconds();
-  return Math.floor((milliseconds * quantity_of_nums) / 1000);
-}
-
-function getRandomNumberB(quantity_of_nums) {
-  let milliseconds = new Date().getMilliseconds();
-  return Math.floor((milliseconds * quantity_of_nums) / 1000);
-}
-
 export function randomGridPosition() {
   return {
-    x: getRandomNumberA(21) + 1,
-    y: getRandomNumberB(16) + 4,
+    x: Math.floor(Math.random() * 16) + 1,
+    y: Math.floor(Math.random() * 16) + 1,
   };
 }
 
